d969465f688d258b4d5ca9710e2f84d56a676177
Miguel Esteban Flores Sierra <ignx.mfs@gmail.com>
2024-04-03 16:24:03 -0500

    Webhook

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -16,19 +16,17 @@ class Log(db.Model):
     fecha_y_hora = db.Column(db.DateTime, default = datetime.utcnow)
     texto = db.Column(db.TEXT)
 
-# Crear la tabla si no existe
-with app.app_context():
+with app.app_context():   # Crear la tabla si no existe
     db.create_all()
-
-    prueba1 = Log(texto = 'Mensaje de Prueba 1')
-    prueba2 = Log(texto = 'Mensaje de Prueba 2')
-
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
+    #prueba1 = Log(texto = 'Mensaje de Prueba 1')
+    #prueba2 = Log(texto = 'Mensaje de Prueba 2')
+    #db.session.add(prueba1)
+    #db.session.add(prueba2)
+    #db.session.commit()
 
 # Función para ordenar los registros por fecha y hora
 def ordenar_por_fecha_y_hora(registros):
+    #return sorted(registros, key = lambda x: x.id, reverse = False) # Para invertir orden de id
     return sorted(registros, key = lambda x: x.fecha_y_hora, reverse = True)
 
 @app.route('/')
@@ -40,16 +38,35 @@ def index():
     return render_template('index.html', registros=registros_ordenados);
 
 mensajes_log = []
-
 # Función para agregar mensajes y guardar en la base de datos
 def agregar_mensajes_log(texto):
     mensajes_log.append(texto)
-    
-    # Guardar el mensaje en la base de datos
-    nuevo_registro = Log(texto=texto)
+    nuevo_registro = Log(texto=texto) # Guardar el mensaje en la base de datos
     db.session.add(nuevo_registro)
     db.session.commit()
     
+# Token de verificación para la configuración
+TOKEN_SS = "SOFTWARESOLUTIONS"
+
+@app.route('/webhook', methods = ['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        response = recibir_mensajes(request)
+        return response
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+    if challenge and token == TOKEN_SS:
+        return challenge
+    else:
+        return jsonify({'Error' : 'Token Invalido'}),401
+
+def recibir_mensajes(req):
+    return jsonify
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
