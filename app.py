d4e026ca601f9e5dbbece2f1add3245a8a99f66d
Miguel Esteban Flores Sierra <ignx.mfs@gmail.com>
2024-04-03 20:23:03 -0500

    Webhook6

--- app.py
+++ app.py
@@ -61,17 +61,12 @@ def verificar_token(req):
         return jsonify({'error':'Token Invalido'}),401
 
 def recibir_mensajes(req):
-    #req = request.get_json()
-    #agregar_mensajes_log(req)
     try:
         req = request.get_json()
         entry = req['entry'][0]
         changes = entry['changes'][0]
         value = changes['value']
         objeto_mensaje = value['messages']
-
-
-
         return jsonify({'message':'EVENT_RECEIVED'})
     except Exception as e:
         return jsonify({'message':'EVENT_RECEIVED'})
