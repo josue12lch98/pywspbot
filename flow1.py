9e1403a2cf7239e43f0312a594483430f951c28d
ignxm <ignx.mfs@gmail.com>
2024-06-30 11:58:09 -0500

    ADD_SUBFLOW2_v5

--- flow1.py
+++ flow1.py
@@ -60,119 +60,7 @@ def handle_flow_1_subflow_1(numero):
     }
     generic_reply(data)
     
-    """
-    data = {
-        "messaging_product": "whatsapp",
-        "to": numero,
-        "type": "interactive",
-        "interactive": {
-            "type": "list",
-            "body": {
-                "text": "Selecciona alguna opción"
-            },
-            "footer": {
-                "text": "Selecciona alguna de las opciones para poder ayudarte"
-            },
-            "action":{
-                "button": "Ver Opciones",
-                "sections": [
-                    {
-                        "title": "Hss",
-                        "rows": [
-                            {
-                                "id": "1 1 1", # FLOW / SUBFLOW / SUBFLOW2
-                                "title": "MIS PAGOS"
-                            },{
-                                "id": "1 1 1", # FLOW / SUBFLOW / SUBFLOW2
-                                "title": "MIS PAGOS"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "XS",
-                        "rows": [
-                            {
-                                "id": "1 1 2",
-                                "title": "INFORMACIÓN SOBRE BENEFICIOS G4S Y OTROS"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "xxxx",
-                        "rows": [
-                            {
-                                "id": "1 1 3",
-                                "title": "PETICIONES"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-     """
-    data = {
-        "messaging_product": "whatsapp",
-        #"recipient_type": "individual",
-        "to": numero,
-        "type": "interactive",
-        "interactive": {
-            "type": "list",
-            "action":{
-                "button": "Ver Opciones",
-                "sections": [
-                    {
-                        "title": "Compra y Venta",
-                        "rows": [
-                            {
-                                "id": "1 1 1",
-                                "title": "Comprar",
-                                "description": "Compra los mejores artículos de tecnología"
-                            },
-                            {
-                                "id": "1 1 2",
-                                "title": "Vender",
-                                "description": "Vende lo que ya no estés usando"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "Distribución y Entrega",
-                        "rows": [
-                            {
-                                "id": "1 1 3",
-                                "title": "Local",
-                                "description": "Puedes visistar nuestro local."
-                            },
-                            {
-                                "id": "1 1 4",
-                                "title": "Entrega",
-                                "description": "La entrega se realiza todo los dias."
-                            }
-                        ]
-                    },
-                    {
-                        "title": "ddd y Entrega",
-                        "rows": [
-                            {
-                                "id": "1 1 5",
-                                "title": "Local",
-                                "description": "Puedes visistar nuestro local."
-                            },
-                            {
-                                "id": "1 1 6",
-                                "title": "Entrega",
-                                "description": "La entrega se realiza todo los dias."
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-    
-    generic_reply(data)
-    """
+
     data = {
         "messaging_product": "whatsapp",
         "recipient_type": "individual",
@@ -180,13 +68,33 @@ def handle_flow_1_subflow_1(numero):
         "type": "interactive",
         "interactive": {
             "type": "button",
-            "action": {
+            "body": {
+                "text": "Selecciona alguna opción"
+            },
+            "footer": {
+                "text": "Selecciona alguna de las opciones para poder ayudarte"
+            },
+            "action":{
                 "buttons": [
                     {
                         "type": "reply",
                         "reply": {
-                            "id": "1 1 0",
+                            "id": "1 1 1",
                             "title": "MIS PAGOS"
+                        }
+                    },
+                    {
+                        "type": "reply",
+                        "reply": {
+                            "id": "1 1 2",
+                            "title": "G4S"
+                        }
+                    },
+                    {
+                        "type": "reply",
+                        "reply": {
+                            "id": "1 1 3",
+                            "title": "PETICIONES"
                         }
                     }
                 ]
@@ -194,5 +102,4 @@ def handle_flow_1_subflow_1(numero):
         }
     }
     generic_reply(data)
-    """
     
